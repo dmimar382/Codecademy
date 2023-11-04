2112718dbe70d2e7768387b20a345892cd780515
dmimar382 <dmimar382@gmail.com>
2023-11-04 10:25:32 -0700

    completed implementation

--- github-oauth-app/app.js
+++ github-oauth-app/app.js
@@ -1,23 +1,19 @@
 /* 
  * Package Imports
 */
-const session = require('express-session');
-const passport = require('passport');
-const GitHubStrategy  = require('passport-github2').Strategy;
-
+const express = require("express");
+const partials = require("express-partials");
+const passport = require("passport");
+const session = require("express-session");
+const GitHubStrategy = require("passport-github2").Strategy;
 const path = require("path");
 require("dotenv").config();
-const express = require('express');
-const partials = require('express-partials');
-
 
 const app = express();
-
 
 /*
  * Variable Declarations
 */
-
 const PORT = 3000;
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
@@ -30,71 +26,83 @@ passport.use(
     {
       clientID: GITHUB_CLIENT_ID,
       clientSecret: GITHUB_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/github/callback'
+      callbackURL: "http://localhost:3000/auth/github/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      done(null, profile);
     }
-  ),
-  (accessToken, refreshToken, profile, done) => {
-    return done(null, profile);
-  }
-)
+  )
+);
 
+ passport.serializeUser((user, done) => {
+  done(null, user);
+ });
 
-
-
-
+ passport.deserializeUser((user, done) => {
+  done(null, user);
+ });
 
 /*
  *  Express Project Setup
 */
+app.set("views", path.join(__dirname, "/views"));
+app.set('view engine', 'ejs');
+app.use(partials());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(
   session({
     secret: "codecademy",
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: false
   })
 );
 app.use(passport.initialize());
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(partials());
-app.use(express.json());
-app.use(express.static(__dirname + '/public'));
-
-
-
+app.use(passport.session());
 
 /*
  * Routes
 */
 
-app.get('/', (req, res) => {
-  res.render('index', { user: req.user });
-})
-
-app.get('/account', (req, res) => {
-  res.render('account', { user: req.user });
+app.get("/", (req, res) => {
+  res.render("index", { user: req.user });
 });
 
-app.get('/login', (req, res) => {
-  res.render('login', { user: req.user });
-})
+app.get("/account", ensureAuthenticated, (req, res) => {
+  res.render("account", { user: req.user });
+});
 
-app.get('/logout', (req, res) => {
+app.get("/login", (req, res) => {
+  res.render("login", { user: req.user });
+});
+
+app.get("/logout", (req, res) => {
   req.logout();
-  res.redirect('/');
+  res.redirect("/");
 });
 
+app.get("/auth/github", passport.authenticate("github", { scope: ["user"] }));
 
-
+app.get(
+  "/auth/github/callback",
+  passport.authenticate("github", {
+    failureRedirect: "/login",
+    successRedirect: "/",
+  })
+);
 
 /*
  * Listener
-*/
-
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+ */
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
 
 /*
  * ensureAuthenticated Callback Function
-*/
-
+ */
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect("/login");
+  }
+}
